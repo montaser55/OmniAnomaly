10667859c079b80aa2db1b50478abdfe35dd4abe
Montaser Majid <montasermajid@207.vpn.tu-cottbus.de>
2025-09-02 15:22:28 +0200

    gpu test code added

--- gpu_test.py
+++ gpu_test.py
@@ -7,11 +7,11 @@ print("Is GPU Available?:", tf.compat.v1.test.is_gpu_available())
 print("GPU Devices:", tf.compat.v1.config.list_physical_devices('GPU'))
 
 # Define a simple matrix multiplication to run on GPU if available
-with tf.device('/GPU:0'):
+with tf.compat.v1.device('/GPU:0'):
     a = tf.compat.v1.constant([[1.0, 2.0], [3.0, 4.0]])
     b = tf.compat.v1.constant([[2.0, 0.0], [0.0, 2.0]])
     c = tf.compat.v1.matmul(a, b)
 
-with tf.Session() as sess:
+with tf.compat.v1.Session() as sess:
     result = sess.run(c)
     print("Matrix Multiplication Result:\n", result)
